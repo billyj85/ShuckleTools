68b226f6ceddfd79fbc5212a4bb7bb25cf367fb4
ShuckleHQ <8nmojd+l1dqz2bvl@sharklasers.com>
2018-01-12 17:03:11 +0100

    fixed args

--- accountloader.py
+++ accountloader.py
@@ -31,6 +31,7 @@ if not args.login:
 duration = datetime.timedelta(hours=int(args.allocation_duration)) if args.allocation_duration else None
 
 location = location_parse(args.location)
+account_manager = AsyncAccountManager.create_empty(args, loop)
 
 
 async def check_account(delay):
@@ -50,8 +51,7 @@ async def check_account(delay):
 num_proxies = len(args.proxy) if args.proxy else 1
 
 async def start():
-    account_manager = AsyncAccountManager.create_empty(args, loop)
-    await account_manager.insert_accounts(monocle_accounts, args.system_id, duration, args.force_system_id,
+    await account_manager.insert_accounts(loop, monocle_accounts, args.system_id, duration, args.force_system_id,
                                     args.skip_assigned, args.overwrite_level)
     print("Inserted accounts")
     await AsyncAccountManager.create_standard(args, loop)

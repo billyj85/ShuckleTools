4448bdd97ff5e0a27d31f339727a1412d48bf5e3
ShuckleHQ <8nmojd+l1dqz2bvl@sharklasers.com>
2018-01-17 22:22:22 +0100

    Upped routes

--- pokestoproutesv2.py
+++ pokestoproutesv2.py
@@ -1,9 +1,11 @@
 from mapelements import RouteElement
 from routes.hamburg_grind1 import spawnpoint_route_1
 from routes.hamburg_grind2 import spawnpoint_route_2
+from routes.hamburg_grind3 import spawnpoint_route_3
 from routes.hamburg_initial import stop_route_initial
 from routes.hamburg_xp1 import xp_route_1
 from routes.hamburg_xp2 import xp_route_2
+from routes.hamburg_xp3 import xp_route_3
 
 initial_130_stops = {"hamburg": list(reversed(RouteElement.from_tuples(stop_route_initial)))}
 
@@ -12,6 +14,10 @@ routes_all = {
         {
             "grind": RouteElement.from_tuples(spawnpoint_route_1),
             "xp": RouteElement.from_tuples(xp_route_1)
+        },
+        {
+            "grind": RouteElement.from_tuples(spawnpoint_route_3),
+            "xp": RouteElement.from_tuples(xp_route_3)
         },
         {
             "grind": RouteElement.from_tuples(spawnpoint_route_2),
